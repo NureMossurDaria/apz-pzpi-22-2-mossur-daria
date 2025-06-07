c6aa855be8a24d6e5a165224dd37ba5add7cebb6
NureMossurDaria <daria.mossur@nure.ua>
2025-06-08 00:38:07 +0300

    Add lab 4

---
+++ Lab4/pzpi-22-2-mossur-daria-lab4/pzpi-22-2-mossur-daria-lab4-code/blooddo-webapp/src/misc/Constants.js
@@ -0,0 +1,7 @@
+export default {
+    API_PATH: 'https://192.168.0.106:14675',
+    TOKEN: 'token',
+    ROLES: 'roles',
+    PI_AGREED: 'pi_agreed',
+    LOGIN_PATH: '/auth/login',
+  };
---
+++ Lab4/pzpi-22-2-mossur-daria-lab4/pzpi-22-2-mossur-daria-lab4-code/blooddo-webapp/src/misc/useAxiosInterceptors.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import axiosInstance from './axiosInstance';
+import CONSTANTS from './Constants';
+import { handleLogout } from './logout';
+
+export const useAxiosInterceptors = (navigate) => {
+    useEffect(() => {
+      const requestInterceptor = axiosInstance.interceptors.request.use(
+        (config) => {
+          const token = localStorage.getItem(CONSTANTS.TOKEN);
+          if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+          }
+          //config.headers['Access-Control-Allow-Origin'] = '*';
+          //config.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT';
+          //config.headers['Access-Control-Allow-Headers'] = 'Content-Type';
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
+        }
+      );
+  
+      const responseInterceptor = axiosInstance.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          if (error.response && (error.response.status === 401)) {
+            handleLogout(navigate);
+          }
+          return Promise.reject(error);
+        }
+      );
+  
+      return () => {
+        axiosInstance.interceptors.request.eject(requestInterceptor);
+        axiosInstance.interceptors.response.eject(responseInterceptor);
+      };
+    }, [navigate]);
+  }
